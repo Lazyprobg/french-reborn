776edfcd795655e6ab2a1f24f554f4e1439ab886
Lazyprobg <urban6134@gmail.com>
2026-01-31 09:42:20 +0100

    Update server.py

--- server.py
+++ server.py
@@ -2,5 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Serveur Lazy actif"
+    return "index.html"
+@app.route("/menu")
+def home():
+    return "menu.html"
+
 app.run(host="0.0.0.0", port=5000)
