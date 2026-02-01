9e7fa3f8ad8cfeb6526ed6d89852edc07bbd8500
Lazyprobg <urban6134@gmail.com>
2026-02-01 13:25:34 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 @app.route("/")
 def home():
@@ -9,5 +9,6 @@ def menu():
 render_template("Menu.html")
 
 app.run(host="0.0.0.0", port=5000)
+
 
 
