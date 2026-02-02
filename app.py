32dba8bce76df4b3bb78e4b793e9bfcf06eb783e
Lazyprobg <urban6134@gmail.com>
2026-02-02 18:10:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
- 
+from flask import Flask
+from flask_sqlalchemy import SQLALchemy
+import os
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLALchemy(app)
+uri = os.environ.get("DATABASE_URL")
+if uri.startswith("postgres://"):
+ uri = uri.replace("postgres://", 'postgresql:// 1)
+
+app.config[SQLALCHEMY_DATABASE_URI"] = uri
