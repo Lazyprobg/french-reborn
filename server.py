4faf76b87dac620f1e21419d0df7b93658f6324a
Lazyprobg <urban6134@gmail.com>
2026-02-02 12:25:40 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
+
 @app.route("/")
 def home():
-    return
-render_template("index.html")
+    return render_template("index.html")
+
 @app.route("/menu")
 def menu():
-    return
-render_template("menu.html")
+    return render_template("menu.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-
-
-
-
-
