00e6cc822101686923cc724d325ea732f1049ab7
Lazyprobg <urban6134@gmail.com>
2026-02-09 23:22:56 +0100

    Refactor server.js and add additional routes
    Refactor server.js to improve structure and add new routes.

--- server/server.js
+++ server/server.js
@@ -1,69 +1,62 @@
+// server/server.js
 import express from "express";
-import session from "express-session";
-import bcrypt from "bcrypt";
-import { db } from "./db.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import { db } from "./db.js"; // Assure-toi que db.js exporte bien `export const db`
 
+// ----------------- Variables globales pour __dirname -----------------
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Sert tous les fichiers du dossier public
+// ----------------- Création de l'application Express -----------------
+const app = express();
+
+// ----------------- Middleware -----------------
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Sert tous les fichiers statiques du dossier public (CSS, JS, images, HTML)
 app.use(express.static(path.join(__dirname, "../public")));
 
+// ----------------- Routes principales -----------------
+
+// Page d'accueil
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-app.use(session({
-  secret: "frenchreborn-secret",
-  resave: false,
-  saveUninitialized: false
-}));
-
-app.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-
-  if (username.length < 5 || username.length > 23)
-    return res.send("Nom invalide");
-
-  const hash = await bcrypt.hash(password, 10);
-
-  db.run(
-    "INSERT INTO users (username, password) VALUES (?, ?)",
-    [username, hash],
-    err => {
-      if (err) return res.send("Nom déjà utilisé");
-      res.redirect("/connexion.html");
-    }
-  );
+// Menu
+app.get("/menu.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/menu.html"));
 });
 
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-
-  db.get(
-    "SELECT * FROM users WHERE username = ?",
-    [username],
-    async (err, user) => {
-      if (!user) return res.send("Utilisateur introuvable");
-
-      const ok = await bcrypt.compare(password, user.password);
-      if (!ok) return res.send("Mot de passe incorrect");
-
-      req.session.user = user;
-
-      if (!user.province) res.redirect("/choosep.html");
-      else res.redirect(`/provinces/${user.province}.html`);
-    }
-  );
+// Connexion
+app.get("/connexion.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/connexion.html"));
 });
 
-app.listen(3000, () => console.log("Serveur lancé"));
+// Inscription
+app.get("/inscription.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/inscription.html"));
+});
 
+// More info
+app.get("/moreinfo.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/moreinfo.html"));
+});
+
+// Choose province
+app.get("/choosep.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/choosep.html"));
+});
+
+// ----------------- Test route -----------------
+app.get("/test", (req, res) => {
+  res.send("Server is running!");
+});
+
+// ----------------- Démarrage du serveur -----------------
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur le port ${PORT}`);
+});
