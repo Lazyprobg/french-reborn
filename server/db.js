de5755d51dac35da49cc2a01c65860f9f11c729c
Lazyprobg <urban6134@gmail.com>
2026-02-09 23:06:20 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,1 +1,21 @@
- 
+ import sqlite3 from "sqlite3";
+
+export const db = new sqlite3.Database("./database.db", (err) => {
+  if (err) {
+    console.error("Erreur DB :", err.message);
+  } else {
+    console.log("Base de données connectée");
+  }
+});
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT UNIQUE,
+      password TEXT,
+      province TEXT DEFAULT NULL,
+      role TEXT DEFAULT 'Citoyen'
+    )
+  `);
+});
