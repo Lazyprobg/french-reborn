f1ac0aa8308e6b97eb552ceb14d50bec37bd4550
Lazyprobg <urban6134@gmail.com>
2026-02-09 23:14:25 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,18 @@ import express from "express";
 import session from "express-session";
 import bcrypt from "bcrypt";
 import { db } from "./db.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Sert tous les fichiers du dossier public
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 const app = express();
 
@@ -54,3 +66,4 @@ app.post("/login", (req, res) => {
 });
 
 app.listen(3000, () => console.log("Serveur lancé"));
+
