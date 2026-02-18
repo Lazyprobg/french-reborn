a7fa27dc78e23ea1b7c2e6faf61d465d04238795
Lazyprobg <urban6134@gmail.com>
2026-02-19 00:23:11 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,21 +1,8 @@
- import sqlite3 from "sqlite3";
+const { Pool } = require("pg");
 
-export const db = new sqlite3.Database("./database.db", (err) => {
-  if (err) {
-    console.error("Erreur DB :", err.message);
-  } else {
-    console.log("Base de données connectée");
-  }
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      username TEXT UNIQUE,
-      password TEXT,
-      province TEXT DEFAULT NULL,
-      role TEXT DEFAULT 'Citoyen'
-    )
-  `);
-});
+module.exports = pool;
